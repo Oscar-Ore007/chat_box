501ec71d69028336bed356b0d17b3f22cbf66a19
Oscar Ore <oscar.ore2011@gmail.com>
2021-12-05 21:31:12 -0500

    creating login page

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+
+const Login = () => {
+    return (
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to ChatBox! </h2>
+            </div>
+        </div>
+    );
+}
+
+export default Login; 

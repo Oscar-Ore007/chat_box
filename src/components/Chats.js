4817f99646e8789d468ecbd867a9928a6e2ad984
Oscar Ore <oscar.ore2011@gmail.com>
2021-12-12 23:15:48 -0500

    creating new users with the help of chatengine api

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useRef, useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 import { auth } from '../firebase';
 
 import { useAuth } from '../contexts/AuthContext'
+import axios from 'axios';
 
 
 const Chats = () => {
@@ -16,6 +17,20 @@ const Chats = () => {
 
         history.push('/');
     }
+
+    useEffect(() => {
+        if(!user) {
+            history.push('/');
+
+            return; 
+        }
+        axios.get('https://api.chatengine.io/users/me', {
+            headers: {
+                "project-id": "c9b25fb4-38b6-4d68-9b3d-c0f1980ae7e4"
+                "user-name": user.email, 
+            }
+        })
+    }, [user,history]);
     return (
         <div className="chats-page">
             <div className="nav-bar">

03313decd9f5211a6aa9ff4de573a82f5d439451
Oscar Ore <oscar.ore2011@gmail.com>
2021-12-12 21:13:04 -0500

    make sure to hide keys

---
+++ src/components/Chats.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth } from '../firebase';
+
+
+const Chats = () => {
+    return (
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Chatbox
+
+                </div>
+                <div className="logo-tab">
+                    Logout 
+                </div>
+            </div>
+
+            <ChatEngine
+                height="calc(100vh - 66px)"
+                projectId="c9b25fb4-38b6-4d68-9b3d-c0f1980ae7e4"
+                userName="."
+                userSecret="."
+                /> 
+
+        </div>
+    )
+}
+
+export default Chats 
